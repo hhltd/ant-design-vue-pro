28f09ce5931d31fbfa6a32eaf837333b35ca394d
hhltd <1608502163@qq.com>
2021-06-06 17:27:52 +0800

    feature/login

--- src/views/User/Login.vue
+++ src/views/User/Login.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>登录页</div>
+  <div>
+    <div>登录页</div>
+    <p>请输入用户名</p>
+    <p>请输入密码</p>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
